43656764ba4f87288e852aefcb4663c551c1facc
Nick Jonas <njonas@sidewalklabs.com>
2021-12-11 19:18:37 -0500

    animations, side projects

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    
+    // var animateBorders = anime({
+    //     targets: '.item',
+    //     opacity: '1',
+    //     autoplay: true,
+    //     duration:2000,
+    //     delay: anime.stagger(50)
+    //   });
+
+
+    // var morphing = anime({
+    //     targets: '.polymorph',
+    //     points: [
+    //         { value: '0,1000 0,0 1000,0 0,0' },
+    //         { value: '300,1000 0,1000 0,0 1000,0 300,0' }
+            
+    //     ],
+    //     easing: 'easeOutQuad',
+    //     duration: 1200,
+    //     loop: false
+    // });
+
+}
